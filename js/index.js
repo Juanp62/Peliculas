a3a7c6d2d0707449b47e0d57673e3cd8100837b2
juanp62m <juanpablatovarromero@gmail.com>
2022-01-12 11:43:09 -0500

    Cambios en el codigo de ventana modal

--- js/index.js
+++ js/index.js
@@ -6,58 +6,59 @@ $(document).ready(function() {
 
 
     fetch(URL).then(response => response.json()).then(results => {
+
+          
+
         let a = this.original_title = results;
         let b = a.results;
         //console.log(b)
-        for(let i=0; i<20;  i++){    
+        for(let i=0; i<b.length;  i++){    
             if(b[i].backdrop_path == null){
                 $('#p').append(`<div class="card">`+`<img src="/../src/img/broken-image.png" />`+`<div class="cont-sub"><h2   class="sub">${b[i].name}</h2><input class="id-movie" type="hidden"  value="${b[i].id}"></div></div>`) 
             }else{
                 $('#p').append(`<div class="card">`+`<img src="https://image.tmdb.org/t/p/w500${b[i].backdrop_path}" />`+`<div class="cont-sub"><a class="sub cta">${b[i].name}</a><input class="id-movie" type="hidden"  value="${b[i].id}"></div></div>`) 
             }
         }
+
+
+        //informacion ventana modal
         $('.sub').click(function(){
             let id_m = $('.id-movie').val();
             const URL_season = `${BASEURL}${id_m}?api_key=${API_KEY}`;
             fetch(URL_season).then(response => response.json()).then(results => {
                 let a = results;
-                //sconsole.log(a.seasons[0].name)
                 let season = a.seasons
-                // console.log (a)
-                $('.cont-mod').append(`<div class="mod.text" ><img src="https://image.tmdb.org/t/p/w500${a.poster_path}"/></div>`);
+                
+                $('.mod-text').append(`<img src="https://image.tmdb.org/t/p/w500${a.poster_path}"/>`);
                 for (let i = 0; i < season.length; i++) {
-                    console.log(season[i].name)                    
-                    $('.cont-mod').append(`<button value="${season[i].name}" class="btn-season" >${season[i].name}</button>`)
+                    $('.mod-text').append(`<input class="btn-season" value="${season[i].name}"  type="button" >`)
+                    
                 }
-                $('.btn-season').click(function(){
-                    let btn_season = $('.btn-season').val();
-                    console.log(btn_season)
-                })
+                let btn_id = $('.btn-season').val();
+                console.log(btn_id)
+                
+                // $('.btn-season').click(function(){
+            
+                // })
+                
             } )              
         })
 
-       
         //Ventana modal
-        let cerrar = document.querySelectorAll(".close")[0];
-        let abrir = document.querySelectorAll(".cta")[0];
-        let modal = document.querySelectorAll(".modal")[0];
-        let modalC = document.querySelectorAll(".modal-container")[0];
-        
-        abrir.addEventListener ("click", function(e) {
+        $('.cta').click(function(e){
             e.preventDefault();
-            modalC.style.opacity = "1";
-            modalC.style.visibility = "visible";
-            modal.classList.toggle("modal-close");
-            
+            $('.modal-container').css({'opacity': '1', "visibility" : "visible"})
+            $('.modal').removeClass('modal-close')
         })
-
-        cerrar.addEventListener("click", function(){
-            modal.classList.toggle("modal-close");
+        
+        $('.close').click(function(){
+            $('.modal').addClass('modal-close')
             setTimeout(function(){
-                modalC.style.opacity = "0";
-                modalC.style.visibility = "hidden";
+                $('.modal-container').css({'opacity': '0', "visibility" : "hidden"})
             }, 900)
-        })        
+        })
+        
+     
     })
 
    
